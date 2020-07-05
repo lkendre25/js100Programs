5f06556518b3673d1529c781a08d8d177b376abc
lkendre25 <lkendre5652@gmail.com>
2020-07-06 00:59:05 +0530

    Finding greater number

--- PositiveNumber.js
+++ PositiveNumber.js
@@ -1,22 +1,10 @@
-
-function sumOfNNaturalnumber(s){
-	let ss=0;
-	for(let i=1;i<=s;i++){
-		ss+=i;
-			
+// Greater number in between two !!
+function greatestOfTwoNumber(first , second){
+	if(first > second){
+		console.log("First Number is Greater !!! ",first);	
+	}else{
+		console.log("Second Number is Greater : ",second);
 	}
-	console.log("Sum of first n Natural number is : ",ss);
-
 }
-sumOfNNaturalnumber(100);
+greatestOfTwoNumber(100,20);
 
-
-function sumOfNnumber(f,l){
-	let sum=0
-	for(let i=f;i<=l;i++){
-		sum+=i;
-	
-	}
-	console.log("sum is : ",sum);
-}
-sumOfNnumber(1,10);
